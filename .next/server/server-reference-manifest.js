1caa59629df9973547df8f941151d290d3d1d823
muhammedshamilmt <muhammedhishamptb@gmail.com>
2026-04-26 11:07:03 +0530

    intiial commit

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"RI609Ylu4ZKeqcovh7XdTPsIR1nJB/d75YqaXXKItYQ=\"\n}"
